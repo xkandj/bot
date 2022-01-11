3b4ab38f6173121702e876628c631eda8c543f47
JosiahMg <JosiahMg@163.com>
2022-01-11 17:19:12 +0800

    fixed bug shell

--- rasa/start_servers.sh
+++ rasa/start_servers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e 
 
 function isCmdExist()
 {
@@ -22,18 +23,19 @@ function createVirtualEnv()
     if [ $? -ne 0 ]; then
         echo "virtualenv not exist, pip install now ..."
         pip install virtualenv==20.10.0
-    else
-        if [ -f "$(pwd)/rasa_ep/runenv/bin/activate" ]; then
-            echo "virtualenv already existed"
-            source "$(pwd)/rasa_ep/runenv/bin/activate"
-        else
-            echo "virtualenv not exist, create now ..."
-            virtualenv "$(pwd)/rasa_ep/runenv"
-            source "$(pwd)/rasa_ep/runenv/bin/activate"
-            pip install -r rasa_ep/requirements.txt
-            python3 -m spacy download zh_core_web_sm
-        fi
     fi
+
+    if [ -f "$(pwd)/rasa_ep/runenv/bin/activate" ]; then
+        echo "virtualenv already existed"
+        source "$(pwd)/rasa_ep/runenv/bin/activate"
+    else
+        echo "virtualenv not exist, create now ..."
+        virtualenv "$(pwd)/rasa_ep/runenv"
+        source "$(pwd)/rasa_ep/runenv/bin/activate"
+        pip install -r rasa_ep/requirements.txt
+        python3 -m spacy download zh_core_web_sm
+    fi
+    
     return 0
 }
 
